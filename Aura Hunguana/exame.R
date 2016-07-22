3fd21b71a86de86e4b82a1280d5dc000c4fc152e
Joe Brew <joebrew@gmail.com>
2016-07-22 12:30:24 +0200

    done grading for Aura

--- Aura Hunguana/exame.R
+++ Aura Hunguana/exame.R
@@ -3,33 +3,54 @@
 #######################################
 dim(BaseDados)
 #1704
+
+#--------------------------------------
+# COMENTARIO DE JOE: PERFECT
+#--------------------------------------
 #######################################
 # 2. How many columns are in df?
 #######################################
 dim(BaseDados)
+#--------------------------------------
+# COMENTARIO DE JOE: PERFECT
+#--------------------------------------
 
 #######################################
 # 3. What are the column names of df?
 #######################################
 names(BaseDados)
-
+#--------------------------------------
+# COMENTARIO DE JOE: PERFECT
+#--------------------------------------
 #######################################
 # 4. What are the different sport in BaseDados?
 #######################################
 # (Hint: use the unique() function)
 
 unique(BaseDados$Desporto)
+#--------------------------------------
+# COMENTARIO DE JOE: PERFECT
+#--------------------------------------
 # 5. Create in object called fotbolista.
 #    This should be all the observations for which
 #    the Desporto is football .
 #######################################
 # (Hint: use the subset() function)
 fotbolista<-subset(BaseDados,Desporto=='FootBall')
+#--------------------------------------
+# COMENTARIO DE JOE: PERFECT
+#--------------------------------------
 # 6. What was the maximum idade in fotbolista?
 #######################################
 max(fotbolista$idade)
+#--------------------------------------
+# COMENTARIO DE JOE: PERFECT
+#--------------------------------------
 #7. Create a plot of the relationship between 
 #     idade and desporto
 #     using only data from BaseDados
 #######################################
 plot(BaseDados$idade,BaseDados$Desporto)
+#--------------------------------------
+# COMENTARIO DE JOE: PERFECT
+#--------------------------------------
--- Aura Hunguana/modulo4activity_exercicio.R
+++ Aura Hunguana/modulo4activity_exercicio.R
@@ -13,22 +13,35 @@ df <- gapminder
 #######################################
 dim(gapminder)
 #1704
+#--------------------------------------
+# COMENTARIO DE JOE: PERFECT
+#--------------------------------------
 #######################################
 # 2. How many columns are in df?
 #######################################
 
 #10
+
+#--------------------------------------
+# COMENTARIO DE JOE: WHERE IS THE CODE YOU USED HERE?
+#--------------------------------------
 #######################################
 # 3. What are the column names of df?
 #######################################
 names(gapminder)
 
+#--------------------------------------
+# COMENTARIO DE JOE: PERFECT
+#--------------------------------------
 #######################################
 # 4. What are the different countries in df?
 #######################################
 # (Hint: use the unique() function)
 
 unique(df$country)
+#--------------------------------------
+# COMENTARIO DE JOE: PERFECT
+#--------------------------------------
 
 #######################################
 # 5. Create in object called df2007.
@@ -37,49 +50,69 @@ unique(df$country)
 #######################################
 # (Hint: use the subset() function)
 df2007<-subset(df,year=='2007')
-
+#--------------------------------------
+# COMENTARIO DE JOE: PERFECT
+#--------------------------------------
 #######################################
 # 6. What was the population of Mozambique in 2007?
 #######################################
 # (Hint: use the subset() function and the df2007 object)
 subset(df2007,country=="Mozambique")
+#--------------------------------------
+# COMENTARIO DE JOE: PERFECT
+#--------------------------------------
 #######################################
 # 7. What was the maximum life expectancy of any country in 2007?
 #######################################
 # (Hint: use the max() function and the df2007 object)
 max(df2007$lifeExp)
 #82.603
-
+#--------------------------------------
+# COMENTARIO DE JOE: PERFECT
+#--------------------------------------
 #######################################
 # 8. What was the average life expectancy of all countries in 2007?
 #######################################
 # (Hint: use the mean() function and the df2007 object)
 mean(df2007$lifeExp)
+#--------------------------------------
+# COMENTARIO DE JOE: PERFECT
+#--------------------------------------
 #######################################
 # 9. Create a histogram of the populations of all countries
 #    in 2007.
 #######################################
 hist(df2007$pop)
+#--------------------------------------
+# COMENTARIO DE JOE: PERFECT
+#--------------------------------------
 #######################################
 # 10. Create a plot of the relationship between 
 #     life expectancy and GDP per capita
 #     using only data from 2007
 #######################################
 plot(df2007$lifeExp,df2007$gdpPercap)
-
+#--------------------------------------
+# COMENTARIO DE JOE: PERFECT
+#--------------------------------------
 #######################################
 # 11. Create an object called moz.
 #     This will be only those observations for
 #     which the country is Mozambique
 #######################################
 moz<-subset(df,country=="Mozambique")
+#--------------------------------------
+# COMENTARIO DE JOE: PERFECT
+#--------------------------------------
 #######################################
 # 12. Make a plot of the year and the life expectancy of Mozambique
 #     (x axis = year, y axis = life expectancy)
 #######################################
 # (Hint: use the moz object)
 plot(moz$year,moz$lifeExp, xlab="yaer",ylab="life expectancy")
-
+#--------------------------------------
+# COMENTARIO DE JOE: PERFECT
+#--------------------------------------
 #######################################
 # 13. Do the same as number 12, but add
 #     a type = 'l' argument to make it a line chart
@@ -89,11 +122,16 @@ plot(moz$year,moz$lifeExp, xlab="yaer",ylab="life expectancy")
 #######################################
 plot(moz$year,moz$lifeExp,type = 'l',col="pink",main="Somos Mocambicanos", cex = 1.5, xlab="yaer",ylab="life expectancy")
 
+#--------------------------------------
+# COMENTARIO DE JOE: PERFECT
+#--------------------------------------
 #######################################
 # 14. Make a plot of the year and population of Mozambique
 #######################################
 plot(moz$year,moz$pop, xlab="yaer",ylab="population of Mozambique")
-
+#--------------------------------------
+# COMENTARIO DE JOE: PERFECT
+#--------------------------------------
 #######################################
 # 15. When you are done with this exercise, 
 #     save this script and email it to joebrew@gmail.com
